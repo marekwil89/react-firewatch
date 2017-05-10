d051d5f1cd17a9dcf62e046dfdccbce9cc901e7a
marekwil89 <marekwil89@gmail.com>
2017-05-10 19:47:50 +0200

    all in one

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.sass$/,
+                include: SRC_DIR,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }]
+            },
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            },
+            { 
+                test: /\.(png|jpg)$/,
+                include: SRC_DIR,
+                loader: "url-loader"
+            }
+        ]
+    }
+};
+
+module.exports = config;
+
